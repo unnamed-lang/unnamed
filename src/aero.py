8c62212553422de477c95fd45678648b34f8bd9b
filippo-fonseca <filifonsecacagnazzo@gmail.com>
2020-11-28 17:02:41 -0600

    feat(core): Add custom Error handling class

--- src/aero.py
+++ src/aero.py
@@ -1,7 +1,32 @@
 #######################################
-# TOKENS
+# CONSTANTS
 #######################################
 
+DIGITS = "0123456789"
+
+#######################################
+# ERRORS
+#######################################
+
+
+class Error:
+    def __init__(self, error_name, details):
+        self.error_name = error_name
+        self.details = details
+
+    def as_string(self):
+        result = f'{self.error_name}: {self.details}'
+        return result
+
+
+class IllegalCharError(Error):
+    def __init__(self, details):
+        super().__init__("Illegal character", details)
+
+
+#######################################
+# TOKENS
+#######################################
 TT_INT = 'TT_INT'
 TT_FLOAT = 'FLOAT'
 TT_PLUS = 'PLUS'
@@ -14,7 +39,7 @@ TT_EOF = 'EOF'
 
 
 class Token:
-    def __init__(self, type_, value):
+    def __init__(self, type_, value=None):
         self.type = type_
         self.value = value
 
@@ -22,6 +47,7 @@ class Token:
         if self.value:
             return f'{self.type}:{self.value}'
         return f'{self.type}'
+
 
 #######################################
 # LEXER
@@ -42,27 +68,53 @@ class Lexer:
     def make_tokens(self):
         tokens = []
 
+
 # Mathematical operations and symbol handling/checking by Aero:
         while self.current_char != None:
             if self.current_char in ' \t':
                 self.advance()
-            elif self.current_char == "+":
+            elif self.current_char in DIGITS:
+                tokens.append(self.make_number)
+            elif self.current_char == '+':
                 tokens.append(Token(TT_PLUS))
                 self.advance()
-            elif self.current_char == "-":
+            elif self.current_char == '-':
                 tokens.append(Token(TT_MINUS))
                 self.advance()
-            elif self.current_char == "*":
+            elif self.current_char == '*':
                 tokens.append(Token(TT_MUL))
                 self.advance()
-            elif self.current_char == "/":
+            elif self.current_char == '/':
                 tokens.append(Token(TT_DIV))
                 self.advance()
-            elif self.current_char == "(":
+            elif self.current_char == '(':
                 tokens.append(Token(TT_LPAREN))
                 self.advance()
-            elif self.current_char == ")":
+            elif self.current_char == ')':
                 tokens.append(Token(TT_RPAREN))
                 self.advance()
+            else:
+                char = self.current_char
+                self.advance()
+                return [], IllegalCharError("'" + char + "'")
 
-        return tokens
+        return tokens, None
+
+    def make_number(self):
+        num_str = ''
+        dot_count = 0
+
+        while self.current_char != None and self.current_char in DIGITS + '.':
+            if self.current_char == '.':
+                if dot_count == 1:
+                    break
+                dot_count += 1
+                num_str += '.'
+            else:
+                num_str += self.current_char
+            self.advance()
+
+        if dot_count == 0:
+            return Token(TT_INT, int(num_str))
+        else:
+            return Token(TT_FLOAT, float(num_str))
