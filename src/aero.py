53d7f0c639929356a6d50c5396a0bb10405e52fc
filippo-fonseca <filifonsecacagnazzo@gmail.com>
2020-11-28 17:16:07 -0600

    refactor(core): Add sync + proper logic for error handling - number engine

--- src/aero.py
+++ src/aero.py
@@ -24,7 +24,7 @@ class Error:
 
 class IllegalCharError(Error):
     def __init__(self, pos_start, pos_end, details):
-        super().__init__(pos_start, pos_end, 'Illegal Character', details)
+        super().__init__(pos_start, pos_end, 'AERO Warning: Illegal Character', details)
 
 #######################################
 # POSITION
