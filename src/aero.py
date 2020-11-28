45a92ec93cb2164dc9952959dc2bceacc26f53f2
filippo-fonseca <filifonsecacagnazzo@gmail.com>
2020-11-28 15:06:24 -0600

    feat(core): Start lexer class - basic

--- src/aero.py
+++ src/aero.py
@@ -14,11 +14,36 @@ TT_EOF = 'EOF'
 
 
 class Token:
-    def __init__(self, type_, _value):
-        self.type_ = type_
-        self._value = _value
+    def __init__(self, type_, value):
+        self.type = type_
+        self.value = value
 
     def __repr__(self):
         if self.value:
             return f'{self.type}:{self.value}'
-            return f'{self.type}'
+        return f'{self.type}'
+
+#######################################
+# LEXER
+#######################################
+
+
+class Lexer:
+    def __init__(self, text):
+        self.text = text
+        self.pos = -1
+        self.current_char = None
+
+    def advance(self):
+        self.pos += 1
+        self.current_char = self.text[self.pos] if self.pos < len(
+            self.text) else None
+
+    def make_tokens(self):
+        tokens = []
+
+        while self.current_char != None:
+            if self.current_char in ' \t':
+                self.advance()
+
+        return tokens
