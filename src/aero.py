8cc8e2e0c122638867fa6def6d487ce94a7cd0bc
filippo-fonseca <filifonsecacagnazzo@gmail.com>
2020-11-28 22:34:27 -0600

    feat(core): Begin work on parser for math logic

--- src/aero.py
+++ src/aero.py
@@ -148,6 +148,67 @@ class Lexer:
         else:
             return Token(TT_FLOAT, float(num_str))
 
+
+#######################################
+# NODES
+#######################################
+
+class NumberNode:
+    def __init__(self, tok):
+        self.tok = tok
+
+    def __repr__(self):
+        return f'{self.tok}'
+
+
+class BinOpNode:
+    def __init__(self, left_node, op_node, right_node):
+        self.left_node = left_node
+        self.op_node = op_node
+        self.right_node = right_node
+
+    def __repr__(self):
+        return f'{self.left_node}, {self.op_node}, {self.right_node}'
+
+#######################################
+# PARSER
+#######################################
+
+
+class Parser:
+    def __init__(self, tokens):
+        self.tokens = tokens
+        self.tok_idx = -1
+
+    def advance(self):
+        self.tok_idx += 1
+        if self.tok_idx >= len(self.tokens):
+            self.current_tok = self.tokens[self.tok_idx]
+        return self.current_tok
+
+    def factor(self):
+        tok = self.current_tok
+
+        if tok.type in (TT_INT, TT_FLOAT):
+            self.advance()
+            return NumberNode(tok)
+
+    def term(self):
+
+    def expr():
+        pass
+
+    def bin_op(self, func, ops):
+        left = func()
+
+        while self.current_tok in ops:
+            op_tok = self.current_tok
+            right = func()
+            left = BinOpNode(left, op_tok, right)
+
+        return left
+
+
 #######################################
 # RUN
 #######################################
